7011f4b3264268fe8513efc90b0cbd285ad9e195
yudinvictor <yudin.production@gmail.com>
2022-04-08 22:01:07 +0300

    mobile support

--- landing/src/app/form/form.component.ts
+++ landing/src/app/form/form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormService} from "./form.service";
 
 @Component({
   selector: 'app-form',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormComponent implements OnInit {
 
-  constructor() { }
+  constructor(public form: FormService) { }
 
   ngOnInit(): void {
   }
